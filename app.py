f1a097436946c8f077cc94fef6116308f284e4fa
Ankeshansh <125234952+Ankeshansh@users.noreply.github.com>
2024-07-05 16:39:47 +0530

    web

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import cv2
 import os
 
 # Download the model weights
-url1 = 'https://drive.google.com/file/d/1yOPZyA14pBrInbM2jS63FX2J8lYpy0Sx/view?usp=drive_link'
+url1 = 'https://drive.google.com/uc?id=1yOPZyA14pBrInbM2jS63FX2J8lYpy0Sx'
 output1 = 'ssd_model_weights.pth'
 
 gdown.download(url1, output1, quiet=False)
